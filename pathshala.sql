adf93ac86798af63dc3c2d41ee8538f7273ea687
Forhad Reza <75982069+F-Reza@users.noreply.github.com>
2022-10-02 20:39:33 +0600

    Add files via upload

---
+++ pathshala.sql
@@ -0,0 +1,360 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 02, 2022 at 04:38 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pathshala`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', '$2y$10$ieFWEBgX8f/bHZ1fc6jIpe.O7GWCwhM9u7Z4sEj5w7QqyWqVF9K2a');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cost`
+--
+
+CREATE TABLE `cost` (
+  `cost_id` int(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `amount` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cost`
+--
+
+INSERT INTO `cost` (`cost_id`, `title`, `amount`) VALUES
+(2, 'Utility Bill', '2500'),
+(3, 'WiFi Bill', '2500');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `c_id` int(255) NOT NULL,
+  `course_title` varchar(255) NOT NULL,
+  `course_teacher_day` varchar(255) NOT NULL,
+  `course_teacher_evening` varchar(255) NOT NULL,
+  `limits` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`c_id`, `course_title`, `course_teacher_day`, `course_teacher_evening`, `limits`) VALUES
+(2, 'Web Programming', 'Farhan Morshed', 'Farhan Morshed', 10),
+(3, 'Mobile Application', 'Farhan Morshed', 'Farhan Morshed', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(255) NOT NULL,
+  `s_id` int(255) NOT NULL,
+  `amount` varchar(255) NOT NULL,
+  `date_time` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `staff`
+--
+
+CREATE TABLE `staff` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `designation` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `salary` varchar(255) NOT NULL,
+  `date_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `staff`
+--
+
+INSERT INTO `staff` (`id`, `name`, `designation`, `user_name`, `salary`, `date_time`) VALUES
+(1, 'Rasel Mahmud', 'Manager', 'rasel', '8000', '2022-09-13 18:45:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `course` varchar(255) NOT NULL,
+  `section` varchar(255) NOT NULL,
+  `total_payment` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `status` enum('1','0') NOT NULL DEFAULT '1',
+  `date_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `students`
+--
+
+INSERT INTO `students` (`id`, `name`, `address`, `phone`, `course`, `section`, `total_payment`, `user_name`, `status`, `date_time`) VALUES
+(5, 'Arko', 'Sewrapara, Mirpur', '01830996044', 'Mobile Application', 'Day', '10000', 'arko', '1', '2022-10-02 18:06:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supervisor`
+--
+
+CREATE TABLE `supervisor` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `qualification` varchar(255) NOT NULL,
+  `taking_course` varchar(255) NOT NULL,
+  `salary` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `date_time` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `supervisor`
+--
+
+INSERT INTO `supervisor` (`id`, `name`, `qualification`, `taking_course`, `salary`, `user_name`, `date_time`) VALUES
+(1, 'Farhan Morshed', 'Developer', 'Web Programming', '10000', 'farhan', '2022-09-13 18:49:12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_spv`
+--
+
+CREATE TABLE `user_spv` (
+  `spv_id` int(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_spv`
+--
+
+INSERT INTO `user_spv` (`spv_id`, `user_name`, `password`) VALUES
+(1, 'farhan', 'farhan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_std`
+--
+
+CREATE TABLE `user_std` (
+  `std_id` int(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_std`
+--
+
+INSERT INTO `user_std` (`std_id`, `user_name`, `password`) VALUES
+(5, 'arko', 'arko');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_stf`
+--
+
+CREATE TABLE `user_stf` (
+  `stf_id` int(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_stf`
+--
+
+INSERT INTO `user_stf` (`stf_id`, `user_name`, `password`) VALUES
+(1, 'rasel', 'rasel');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `cost`
+--
+ALTER TABLE `cost`
+  ADD PRIMARY KEY (`cost_id`),
+  ADD UNIQUE KEY `title` (`title`);
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`c_id`),
+  ADD UNIQUE KEY `title` (`course_title`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user` (`user_name`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user` (`user_name`);
+
+--
+-- Indexes for table `supervisor`
+--
+ALTER TABLE `supervisor`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user` (`user_name`);
+
+--
+-- Indexes for table `user_spv`
+--
+ALTER TABLE `user_spv`
+  ADD PRIMARY KEY (`spv_id`),
+  ADD UNIQUE KEY `user` (`user_name`);
+
+--
+-- Indexes for table `user_std`
+--
+ALTER TABLE `user_std`
+  ADD PRIMARY KEY (`std_id`),
+  ADD UNIQUE KEY `user` (`user_name`);
+
+--
+-- Indexes for table `user_stf`
+--
+ALTER TABLE `user_stf`
+  ADD PRIMARY KEY (`stf_id`),
+  ADD UNIQUE KEY `user` (`user_name`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cost`
+--
+ALTER TABLE `cost`
+  MODIFY `cost_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `c_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `staff`
+--
+ALTER TABLE `staff`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `supervisor`
+--
+ALTER TABLE `supervisor`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_spv`
+--
+ALTER TABLE `user_spv`
+  MODIFY `spv_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_std`
+--
+ALTER TABLE `user_std`
+  MODIFY `std_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `user_stf`
+--
+ALTER TABLE `user_stf`
+  MODIFY `stf_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
